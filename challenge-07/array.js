cd0676ce31e64784e888e26fc2350d1202bcf91e
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-11-13 15:14:36 -0200

    completed challeng-07

---
+++ challenge-07/array.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5]
+
+console.log(arr.length)
+
+arr.push(10)
+
+
+console.log(arr)
+arr.push([12,23,43])
+console.log(arr)
+
+console.log(arr[6][1])
+
---
+++ challenge-07/for.js
@@ -0,0 +1,4 @@
+for (var num = 0; num < 20; num++) {
+    console.log('num', num) 
+}
+
